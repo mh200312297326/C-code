c6c927fb5bde4e578cf2537514851e0eb538976d
Meng <3233114232@qq.com>
2022-03-17 15:44:10 +0800

    oj第二次作业

---
+++ Project6/Project6/text.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//float f(float x)
+//{
+//	if (x < 1)
+//		return x;
+//	else if ((x >= 1) && (x <= 10))
+//		return (2 * x - 1);
+//	else
+//		return (3 * x - 11);
+//}
+//
+//int main()
+//{
+//	float x = 0;
+//	scanf("%f", &x);
+//	printf("y=%.2f\n", f(x));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char x = getchar();
+//	if ((x >= 'A') && (x <= 'Z'))
+//		x += 32;
+//	printf("%c\n", x);
+//	return 0;
+//}
+
+//int main()
+//{
+//	double w = 0;
+//	scanf("%lf", &w);
+//	if (w >= 10000)
+//		printf("Tax=%.2lfyuan\n", (0.05 * w));
+//	else if ((w >= 5000) && (w < 10000))
+//		printf("Tax=%.2lfyuan\n", (0.03 * w));
+//	else if ((w >= 1000) && (w < 5000))
+//		printf("Tax=%.2lfyuan\n", (0.02 * w));
+//	else
+//		printf("Tax=0yuan\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	if ((n >= 85) && (n <= 100))
+//		printf("A\n");
+//	else if ((n >= 70) && (n <= 84))
+//		printf("B\n");
+//	else if ((n >= 60) && (n <= 69))
+//		printf("C\n");
+//	else
+//		printf("D\n");
+//	return 0;
+//}
